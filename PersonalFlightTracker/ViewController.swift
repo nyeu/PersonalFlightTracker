d925277d5bacc9608b0194d2e3676f3042417b3e
Joan Cardona <joancardona.17@gmail.com>
2020-08-24 16:26:52 +0200

    Initial Commit

---
+++ PersonalFlightTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalFlightTracker
+//
+//  Created by Joan on 24/08/2020.
+//  Copyright © 2020 Joan Cardona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
